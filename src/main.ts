aee7dca2e1b671d0773de25ea3e572f5b07ffdf9
Liliia Danylenko <liliia.danylenko.dev@gmail.com>
2025-12-30 15:17:16 +0200

    fix request url

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,10 @@ import './lib/actions';
 import './lib/render-stats';
 
 window.API_CONFIG = {
-  baseUrl: import.meta.env.VITE_API_BASE_URL ?? 'https://dashboard-eight-beta-59.vercel.app/api',
-  getEmployees: `${import.meta.env.VITE_API_BASE_URL ?? 'https://dashboard-eight-beta-59.vercel.app/api'}/get-employees`,
-  addViolation: `${import.meta.env.VITE_API_BASE_URL ?? 'https://dashboard-eight-beta-59.vercel.app/api'}/add-violation`,
-  updateData: `${import.meta.env.VITE_API_BASE_URL ?? 'https://dashboard-eight-beta-59.vercel.app/api'}/update-data`
+  baseUrl: import.meta.env.VITE_API_BASE_URL ?? '/api',
+  getEmployees: `${import.meta.env.VITE_API_BASE_URL ?? '/api'}/get-employees`,
+  addViolation: `${import.meta.env.VITE_API_BASE_URL ?? '/api'}/add-violation`,
+  updateData: `${import.meta.env.VITE_API_BASE_URL ?? '/api'}/update-data`
 };
 
 
